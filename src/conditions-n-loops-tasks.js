b287612ac8d358c6fb88f7bfc325aecf6077ca08
Luba Hadunova <luba.godunova2018@gmail.com>
2023-12-19 17:37:27 +0100

    feat: add solution to isPalindrome function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -237,8 +237,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const halfIndex = Math.trunc(str.length / 2);
+  for (let i = 0; i <= halfIndex; i += 1) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
